24346a069ad8a2d7449d0e589ea5e23d8d893f17
FaishalDanni-24 <faishaldanni24@gmail.com>
2026-05-09 21:56:50 +0700

    Folder Structure & Player Movement
    Added Player Movement.
    Added Folder Prefabs, Scripts, and a few files.
    Check commit details for more info.

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rbPlayer;
+
+    public float inputX;
+    public float inputY;
+    public float thrust;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbPlayer = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputX = Input.GetAxis("Horizontal");
+        inputY = Input.GetAxis("Vertical");
+    }
+
+    // FixedUpdate is called once per 20 ms (0.02 s)
+    void FixedUpdate()
+    {
+        rbPlayer.AddForce(transform.up * thrust * inputY);
+        rbPlayer.AddTorque(-(inputX * thrust));
+    }
+}
