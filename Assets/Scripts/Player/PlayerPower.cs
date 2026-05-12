693c7f9accfe53db1468e6c9a429422d9539570c
FaishalDanni-24 <faishaldanni24@gmail.com>
2026-05-12 11:21:19 +0700

    Add Player Health
    Sekarang player punya nyawa dan bisa kurang jika tabrakan dengan gameobject selain (powerup, belum diimplementasi).

--- Assets/Scripts/Player/PlayerPower.cs
+++ Assets/Scripts/Player/PlayerPower.cs
@@ -2,13 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCollision : MonoBehaviour
+public class PlayerPower : MonoBehaviour
 {
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        Debug.Log("Player Collided!");
-        gameObject.GetComponent<PlayerPower>().health -= 1;
-    }
+    // Attributes
+    public int health;
+    public int powerLevel;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +17,6 @@ public class PlayerCollision : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Player Health: " + health);
     }
 }
