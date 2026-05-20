53bfae74ab99cd11f0fe1dfe1428b1e9a03eacfa
FaishalDanni-24 <faishaldanni24@gmail.com>
2026-05-20 17:23:57 +0700

    Koreksi fitur asteroid dan satelit
    Prefab: Asteroid, Comet, dan Buat Small Asteroid
    *Stat: Satelit dan Asteroid

---
+++ Assets/Scripts/Asteroid/AsteroidStat.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidStat : MonoBehaviour
+{
+    public bool comet;
+    public bool smallAst;
+    public int score = 10;
+}
