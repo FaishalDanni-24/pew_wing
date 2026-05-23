9e9c0e3767d8c35fe77ed86f5bc610d9142460ae
FaishalDanni-24 <faishaldanni24@gmail.com>
2026-05-23 10:01:30 +0700

    Dokumentasi pada Script (1)
    Menambahkan comment-comment ke code

--- Assets/Scripts/Asteroid/AsteroidStat.cs
+++ Assets/Scripts/Asteroid/AsteroidStat.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class AsteroidStat : MonoBehaviour
 {
+    // Atribut asteroid
     public bool comet;
     public bool smallAst;
     public int score = 10;
--- Assets/Scripts/Satellite/SatelliteStat.cs
+++ Assets/Scripts/Satellite/SatelliteStat.cs
@@ -4,5 +4,6 @@ using UnityEngine;
 
 public class SatelliteStat : MonoBehaviour
 {
+    // Atribut satelit
     public int score = 100;
 }
